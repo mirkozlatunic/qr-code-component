924c4468a284dd2b8890c1c1fc43a7920bbc286a
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-02-28 12:55:14 -0800

    change vite.config.js to build dist file

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,12 @@ export default {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
     },
   },
+  build: {
+    // Explicitly set the output directory
+    outDir: path.resolve(__dirname, 'dist'),
+    // When using a custom root, you need to adjust where assets are loaded from
+    emptyOutDir: true,
+  },
   server: {
     port: 8080,
     hot: true,
